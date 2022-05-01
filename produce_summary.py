8bda8bec3f0fe493315865aa3e0678908adaf5fb
Megan Quintana <meganquintana30@gmail.com>
2022-04-30 18:43:29 -0700

    Finished code

--- produce_summary.py
+++ produce_summary.py
@@ -1,40 +1,46 @@
-print("Day 1")
-the_file = open("um-deliveries-day-1.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def print_report(day):
+    print(f"Day {day}")
+    the_file = open(f"um-deliveries-day-{day}.txt")
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+        #print("word: ", words)
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+    the_file.close()
 
-
-print("Day 2")
-the_file = open("um-deliveries-day-2.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+for i in range(1, 4):
+    print_report(i)
 
 
-print("Day 3")
-the_file = open("um-deliveries-day-3.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+# print("Day 2")
+# the_file = open("um-deliveries-day-2.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
+
+
+# print("Day 3")
+# the_file = open("um-deliveries-day-3.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
